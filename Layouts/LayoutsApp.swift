da1769cef52955b40a35feb9aed8b1fb826243a1
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-27 16:12:48 -0500

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Nia Mitchell on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
