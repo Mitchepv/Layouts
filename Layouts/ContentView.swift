31809a2b40b0f0139ef23cdaa26e61662b794472
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-27 16:50:50 -0500

    lesson 1 layout

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messagestring = "Hey there! When the genuis bar need help, call me!"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text (messagestring)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange,width: 2)
+                .padding()
+            
+            HStack{
+                
+                Button("Awesome"){
+                    messagestring = "You are Awesome"
+                }
+                
+                Button ("Great!"){
+                    messagestring = "You are Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .border(.purple, width: 5)
+     
         }
-        .padding()
+       
     }
 }
 
