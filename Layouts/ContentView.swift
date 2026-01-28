75b9540ec96b6515be64ae2a310cd6c600a66b15
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-28 12:45:02 -0500

    ch.1 layout part 2

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -11,32 +11,59 @@ struct ContentView: View {
     @State private var messagestring = "Hey there! When the genuis bar need help, call me!"
     
     var body: some View {
-        VStack {
-            Text (messagestring)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange,width: 2)
-                .padding()
+//        ZStack {
+//            Color.yellow
+//                .ignoresSafeArea()
+//        } embedd in vstack
             
-            HStack{
+            VStack {
                 
-                Button("Awesome"){
-                    messagestring = "You are Awesome"
-                }
+//                Text("You have skills")
+//                    .font(.largeTitle)
+//                    .fontWeight(.black)
+//                    .foregroundStyle(.goldBC)
+//                    .padding()
+//                    .background(Color("maroon-BC"))
+//                    .clipShape(RoundedRectangle(cornerRadius: 15 ))
                 
-                Button ("Great!"){
-                    messagestring = "You are Great!"
+                Spacer()
+                
+                Text (messagestring)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .border(.orange,width: 2)
+                    .padding()
+                
+                Spacer()
+                
+                
+                HStack{
+                    
+                    Button("Awesome"){
+                        messagestring = "You are Awesome"
+                    }
+                    
+                    Spacer()
+                    
+                    Button ("Great!"){
+                        messagestring = "You are Great!"
+                    }
                 }
+                .buttonStyle(.borderedProminent)
+                //            m
+                .padding()
+                .tint(.vermillion)
+                
             }
-            .buttonStyle(.borderedProminent)
-            .border(.purple, width: 5)
-     
-        }
+//            .background(
+//                Gradient(colors: [.darkIce, .orange ])
+//            )
+         
        
     }
 }
